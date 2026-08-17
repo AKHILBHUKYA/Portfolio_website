0b169c598afcc0e378df9464b05d74a6245c1001
Akhil Bhukya <akhilbhukya16@.com>
2026-08-17 09:27:30 +0530

    Fix Banner useEffect warning

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,19 +5,19 @@ import headerImg from "../assets/img/header-img.svg";
 import "animate.css";
 import TrackVisibility from "react-on-screen";
 
+const toRotate = [
+  "Software Development Engineer",
+  "Backend Developer",
+  "Java & Spring Boot Developer",
+];
+
+const period = 1800;
+
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(250 - Math.random() * 80);
-
-  const toRotate = [
-    "Software Development Engineer",
-    "Backend Developer",
-    "Java & Spring Boot Developer",
-  ];
-
-  const period = 1800;
 
   useEffect(() => {
     const tick = () => {
